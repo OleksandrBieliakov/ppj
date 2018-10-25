30b09c3856a09d1d2774d9b00f159bbc771c294a
Oleksandr Bieliakov <bel9k@ukr.net>
2018-10-25 23:06:08 +0200

    E3P5 was added

---
+++ src/E3P5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class E3P5 {
+	
+	public static void main (String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		for (int y = 1; y <= n; y++) {
+			
+			for (int x = 1; x <= n; x++) {
+				
+				System.out.printf("%4d", y*x);
+				
+			}  
+			
+			System.out.println();
+			
+		}
+		
+	}
+	
+}
