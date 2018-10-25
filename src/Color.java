b633d2d7bd67a01c3077784473599ced787639bc
Alexander Belyakov <a.belyakov@ameria.de>
2018-10-25 22:40:13 +0300

    Color class added as a sample of bitwise operators

---
+++ src/Color.java
@@ -0,0 +1,16 @@
+public class Color {
+
+    public static void main(String[] args) {
+
+        int argb = 293274;
+        System.out.println("argb to hex = " + Integer.toHexString(argb));
+
+        argb >>>= 8;
+
+        int g = argb & 255;
+
+        System.out.println("  g  to hex =  " + Integer.toHexString(g));
+        System.out.println("g = " + g);
+    }
+
+}
