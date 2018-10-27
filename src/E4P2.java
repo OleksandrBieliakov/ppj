c53f6566210546d1a55532ea48e2b6bf742a6863
Oleksandr Bieliakov <bel9k@ukr.net>
2018-10-27 19:11:06 +0200

    E4P2 added

---
+++ src/E4P2.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class E4P2 {
+
+    public static void main(String[] args) {
+
+        String s = JOptionPane.showInputDialog(null, "Enter an integer");
+
+        int n = Integer.parseInt(s);
+
+		String s1 = "";
+		
+        for (int f = -2147483648, i = 0; i < 31; ++i, f = f >>> 1) {
+            if (n & f != 0)
+                s1 += "1, ";
+			else if (s1 != "")
+				s1 += "0, ";
+		}
+		if (n & 1 != 0)
+			s1 += "1.";
+		else
+			s1 += "0.";
+        
+            JOptionPane.showMessageDialog(null, s1);
+
+    }
+
+}
