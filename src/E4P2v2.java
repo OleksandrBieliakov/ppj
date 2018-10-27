a9fc09c690d15cc77c3fccfb213e066490c48094
Alexander Belyakov <avbelyakov@gmail.com>
2018-10-27 21:53:24 +0300

    use while instead of for

--- src/E4P2v2.java
+++ src/E4P2v2.java
@@ -9,11 +9,14 @@ public class E4P2v2 {
         int n = Integer.parseInt(s);
 
         String s1 = "";
-
-        for (int i = 0; n > 0; ++i, n = n >>> 1) {
-            if (i > 0)
+        boolean first = true;
+        while (n > 0) {
+            if (first)
+                first = false;
+            else
                 s1 += ",";
             s1 += n & 1;
+            n >>>= 1;
         }
 
         JOptionPane.showMessageDialog(null, s1);
