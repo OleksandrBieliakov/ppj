14a95c2b2cb3ad34275d8cc27b48d1efb390e239
Oleksandr Bieliakov <bel9k@ukr.net>
2018-10-27 19:18:28 +0200

    Bug fixed

--- src/E4P2.java
+++ src/E4P2.java
@@ -11,12 +11,12 @@ public class E4P2 {
 		String s1 = "";
 		
         for (int f = -2147483648, i = 0; i < 31; ++i, f = f >>> 1) {
-            if (n & f != 0)
+            if ((n & f) != 0)
                 s1 += "1, ";
 			else if (s1 != "")
 				s1 += "0, ";
 		}
-		if (n & 1 != 0)
+		if ((n & 1) != 0)
 			s1 += "1.";
 		else
 			s1 += "0.";
