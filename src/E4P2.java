83ba80d9abd5dc61638c17d524b1bfbfbf96c174
Oleksandr Bieliakov <bel9k@ukr.net>
2018-10-27 20:05:16 +0200

    Reverse

--- src/E4P2.java
+++ src/E4P2.java
@@ -10,16 +10,20 @@ public class E4P2 {
 
 		String s1 = "";
 		
-        for (int f = -2147483648, i = 0; i < 31; ++i, f = f >>> 1) {
-            if ((n & f) != 0)
+        for (int i = 0; i < 31; ++i, n = n >>> 1) {
+            if (n == 1){
+				s1 += "1.";
+				break;
+			} else if (n == 0) {
+				s1 += "0.";
+				break;
+			} else {
+			if ((n & 1) != 0)
                 s1 += "1, ";
-			else if (s1 != "")
+			else
 				s1 += "0, ";
+			}
 		}
-		if ((n & 1) != 0)
-			s1 += "1.";
-		else
-			s1 += "0.";
         
             JOptionPane.showMessageDialog(null, s1);
 
