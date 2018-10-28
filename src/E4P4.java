4aec7844692f3b82c6364021440667ee1b73b97e
Oleksandr Bieliakov <bel9k@ukr.net>
2018-10-28 11:54:04 +0100

    E4P4 added

---
+++ src/E4P4.java
@@ -0,0 +1,40 @@
+import javax.swing.JOptionPane;
+
+public class E4P4 {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, 
+		"20 Questions game!\nThink of a number from 1 to million.");
+
+		int n = 500000, k = 250000, i = 0;
+		
+        String st = "";
+        
+        while (true) {
+		
+		st = JOptionPane.showInputDialog(null, 
+		"Is this " + n + "?\nAnswer \"s\" if " + n + " is smaller than the chosen number;" + 
+		"\n\"b\" if " + n + " is bigger;\n\"y\" if " + n + " is your number."
+		);
+		
+		++i;
+		
+		if ("y".equals(st))
+			break;
+		if ("s".equals(st))
+			n += k;
+		else if ("b".equals(st))
+			n -= k;
+			
+		k /= 2;
+		
+		}
+		
+		JOptionPane.showMessageDialog(null, 
+		"Your number is " + n + "!\nNumber of trials: " + i);
+
+    }
+	
+}
+
