88b9130ffa43bb0f80170534c61b8e57ea82ce12
Alexander Belyakov <avbelyakov@gmail.com>
2018-10-28 15:07:03 +0300

    20 questions v 2

---
+++ src/E4P4v2.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class E4P4v2 {
+
+    public static void main(String[] args) {
+
+        int i = 0, min = 0, max = 1000000, n;
+
+
+        JOptionPane.showMessageDialog(null,
+                "20 Questions game!\nThink of a number from 1 to " + max);
+
+        while (true) {
+            n = (max + min) / 2;
+
+            String st = JOptionPane.showInputDialog(null,
+                    "Is this " + n + "?\nAnswer \"s\" if " + n + " is smaller than the chosen number;" +
+                            "\n\"b\" if " + n + " is bigger;\n\"y\" if " + n + " is your number."
+            );
+
+            ++i;
+
+            if ("y".equals(st))
+                break;
+            if ("s".equals(st))
+                min = n;
+            else if ("b".equals(st))
+                max = n;
+        }
+
+        JOptionPane.showMessageDialog(null,
+                "Your number is " + n + "!\nNumber of trials: " + i);
+
+    }
+
+}
+
