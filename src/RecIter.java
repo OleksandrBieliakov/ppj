940a833e318de4cbbc9a83a34a4018d1d72fa639
Oleksandr Bieliakov <bel9k@ukr.net>
2018-11-16 23:49:41 +0100

    E9P1 fixed

--- src/RecIter.java
+++ src/RecIter.java
@@ -77,9 +77,9 @@ public class RecIter {
 	public static boolean isPalindrom(String s) {
 		if(s.length() > 1) {
 			if(s.charAt(0) == s.charAt(s.length() - 1)) {
-				isPalindrom(s.substring(1, s.length() - 1));
-			} else return false;
-		}
+				return isPalindrom(s.substring(1, s.length() - 1));
+			} else return false; 
+		} 
 		return true;
 	}
 		
