43b25bf9baeef9e1ce69d58638ae01e1ca514f82
Oleksandr Bieliakov <bel9k@ukr.net>
2018-11-17 01:37:22 +0100

    Shortened

--- src/RecIter.java
+++ src/RecIter.java
@@ -1,10 +1,8 @@
 public class RecIter {
 	
 	public static int fiboR(int n) {
-		int fn = 0;
 		if(0 <= n && n < 2) return n;
-		else fn = fiboR(n-1) + fiboR(n - 2); 
-		return fn;
+		return fiboR(n-1) + fiboR(n - 2); 
 	}
 	
 	public static int fiboI(int n) {
@@ -12,39 +10,31 @@ public class RecIter {
 		int fn1 = 1;
 		int fn2 = 0;
 		if(0 <= n && n < 2) return n;
-		else 
-			for(int i = 2; i <= n; ++i) {
-				fn = fn1 + fn2;
-				fn2 = fn1;
-				fn1 = fn;
-			}
+		for(int i = 2; i <= n; ++i) {
+			fn = fn1 + fn2;
+			fn2 = fn1;
+			fn1 = fn;
+		}
 		return fn;
 	}
 	
 	public static int factR(int n) {
-		int fac = 0;
 		if(n == 0) return 1;
-		else fac = n * factR(n-1);
-		return fac;
+		return n * factR(n-1);
 	}
 	
 	public static int factI(int n) {
-		int fac = 0;
-		int fac1 = 1;
-		if(n == 0) return 1;
-		else
-			for(int i = 1; i <= n; ++i) {
-				fac = i * fac1;
-				fac1 = fac;
-			}
+		int fac = 1;
+		for(int i = 1; i <= n; ++i)
+			fac = i * fac;
 		return fac;	
 	}
 	
 	public static int gcdR(int a, int b) {
+		if(a == b) return a;
 		if(a > b) a -= b;
-		else if(b > a) b -= a;
-		if(a != b) return gcdR(a, b);
-		return a;
+		else b -= a;
+		return gcdR(a, b);
 	}
 
 	public static int gcdI(int a, int b) {
@@ -57,12 +47,10 @@ public class RecIter {
 		
 	public static int maxElem(int[] arr, int from, int to) {
 		if(from == to - 1) return arr[from];
-		else {
         if(Math.max(arr[from], arr[to - 1]) == arr[from])
 			return maxElem(arr, from, --to);
 		else
 			return maxElem(arr, ++from, to);
-		}
 	}
 	
 	public static void reverse(int[] arr, int from, int to) {
