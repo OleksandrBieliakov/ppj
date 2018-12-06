37bbc0ade1c6d49065c7bbb9d38215e87c358c13
Oleksandr Bieliakov <bel9k@ukr.net>
2018-12-06 03:02:20 +0100

    Finished

--- src/MethodCarrier.java
+++ src/MethodCarrier.java
@@ -1,37 +1,42 @@
+import java.util.Scanner;
+
 public class MethodCarrier {
-	
-	public static void main (String[] args) {
-	
-		draw(5);
-	
+
+	public static void innerLoops(int i, int edge) {
+		int n = 0;
+		for (int j = 0; j < edge/2; ++j) {
+			System.out.print(n + " ");
+			if (n < i) ++n;
+		}
+		if (edge%2 != 0) {
+			System.out.print(n + " ");
+		}
+		for (int j = edge/2; j > 0; --j) {
+			if(n >= j) --n;
+			System.out.print(n + " ");
+		}
 	}
+
 	
 	public static void draw(int edge) {
-		
-		for(int i = edge; i > edge/2; --i) {
-			
-			for(int j = edge; j > edge/2; --j) {
-				System.out.print(((edge - i) + (edge -j)) + " ");
-			}
-			for(int j = edge/2; j < edge; ++j) {
-				System.out.print(((edge - i) + (edge -j)) + " ");
-			}
+		for (int i = 0; i < edge/2; ++i) {
+			innerLoops(i, edge);
 			System.out.println();
-		
 		}
-		
-		for(int i = edge/2; i < edge; ++i) {
-			
-			for(int j = edge; j > edge/2; --j) {
-				System.out.print(((edge - i) + (edge -j)) + " ");
-			}
-			for(int j = edge/2; j < edge; ++j) {
-				System.out.print(((edge - i) + (edge -j)) + " ");
-			}
+		if (edge%2 != 0) {
+			innerLoops(edge/2, edge);
 			System.out.println();
-			
 		}
-		
+		for (int i = edge/2; i > 0; --i) {
+			innerLoops(i - 1, edge);
+			System.out.println();
+		}
 	}
-	
+
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int edge = scan.nextInt();
+		draw(edge);
+	}
+
 }
