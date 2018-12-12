dda1e8c9738fe8161323e5be0ae8c0df36357359
Oleksandr Bieliakov <bel9k@ukr.net>
2018-12-12 19:50:05 +0100

    Added

---
+++ src/rocket/Computer.java
@@ -0,0 +1,19 @@
+package rocket;
+
+public class Computer {
+
+    private int attitude;
+    private int step;
+
+    public Computer(int step) {
+        attitude = 0;
+        this.step = step;
+    }
+
+    public int getAttitude() {
+        attitude += step;
+        System.out.print(attitude + " ");
+        return attitude;
+    }
+
+}
---
+++ src/rocket/Main.java
@@ -0,0 +1,17 @@
+package rocket;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rocket rocket = new Rocket();
+
+        try {
+            rocket.start();
+        } catch (ComputerErrorException ex) {
+            System.out.println("Attitude 10000 is reached according to one of the computers -\nattitudes calculated by Computer 1 and 2 are not equal.");
+        }
+
+    }
+
+}
