36b18d581312f1b33f4fa3e68333f36bca7f34d3
Oleksandr Bieliakov <bel9k@ukr.net>
2018-12-21 20:14:22 +0100

    Pointed task from the class

---
+++ src/pointedtask/t1/Main.java
@@ -0,0 +1,25 @@
+package pointedtask.t1;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Rocket rocket = new Rocket("R1", 4000);
+
+        try {
+            rocket.launch();
+        } catch (IllegalArgumentException ex) {
+            System.out.print(ex);
+        }
+
+        rocket.refill();
+
+        try {
+            rocket.launch();
+        } catch (IllegalArgumentException ex) {
+            System.out.print(ex);
+        }
+
+    }
+
+}
