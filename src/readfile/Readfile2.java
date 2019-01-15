c6d0b96c98d7b78e83f427dcf3509af0ec898e3f
Oleksandr Bieliakov <bel9k@ukr.net>
2019-01-15 10:47:59 +0100

    Updated

--- src/readfile/Readfile2.java
+++ src/readfile/Readfile2.java
@@ -8,11 +8,12 @@ public class Readfile2 {
 
     public static void main(String[] args) throws Exception {
         // pass the path to the file as a parameter
-        FileReader fr = new FileReader("C:\\Projects\\Hello.txt");
+        FileReader fr = new FileReader("data/text.txt");
 
         int i;
         while ((i=fr.read()) != -1)
-            System.out.print((char) i);
+            System.out.print((char)i);
+
     }
 
 }
