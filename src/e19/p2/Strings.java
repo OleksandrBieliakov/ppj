bc470dd821d8f1c39a000ebbf1392b4b396cc1b2
Alexander Belyakov <avbelyakov@gmail.com>
2019-01-27 18:34:09 +0300

    read filename and max length of string as a cmd line parameters

--- src/e19/p2/Strings.java
+++ src/e19/p2/Strings.java
@@ -8,11 +8,19 @@ public class Strings {
 
     public static void main(String[] args) {
 
-        int n = 4;
+        if (args.length == 0) {
+            System.out.println("Usage: java Strings fileName [min ASCII string length (default 4)]\n" +
+                    "Example: java Strings Hello.class 5");
+            return;
+        }
+
+        String fileName = args[0];
+        int n = args.length > 1 ? Integer.parseInt(args[1]) : 4;
+
         List list = new List();
 
         try {
-            FileInputStream is = new FileInputStream("data/Hello.class");
+            FileInputStream is = new FileInputStream(fileName);
             int read;
             int count = 0;
             StringBuffer sb = new StringBuffer();
@@ -20,11 +28,9 @@ public class Strings {
                 if(read > 31 && read < 127){
                     sb.append((char)read);
                     count++;
-                } else if(count >= n){
-                    list.add(sb.toString());
-                    sb = new StringBuffer();
-                    count = 0;
                 } else {
+                    if (count >= n)
+                        list.add(sb.toString());
                     sb = new StringBuffer();
                     count = 0;
                 }
