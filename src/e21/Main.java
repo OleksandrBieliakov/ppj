79ac0103426ff3d0561daa29a9eda0ca2ef329bf
Oleksandr Bieliakov <bel9k@ukr.net>
2019-03-08 21:45:14 +0100

    Lim array setting added

--- src/e21/Main.java
+++ src/e21/Main.java
@@ -3,6 +3,7 @@ package e21;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -38,7 +39,6 @@ public class Main {
         return sb.toString();
     }
 
-
     public static void main (String[] args) {
 
         String text = "";
@@ -60,9 +60,20 @@ public class Main {
 
         System.out.println(text + "\n");
 
+        ////////////////////////////////////////////////
 
-        int[] lim = {2, 3, 5, 7};
-        int len = lim.length;
+        Scanner scan = new Scanner(System.in);
+        String limSt = scan.nextLine();
+
+        String[] limStArr = limSt.split("\\s+");
+        int len = limStArr.length;
+        int[] lim = new int[len];
+
+        for(int i = 0; i < len; ++i) {
+            lim[i] = limStArr[i].charAt(0) - '0'; //ASCI trick
+        }
+
+        ////////////////////////////////////////////////
 
         String reg = makeReg(1, lim[0]);
         countPrint(reg, text);
