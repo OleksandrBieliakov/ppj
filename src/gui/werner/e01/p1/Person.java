f4e263b19fcb005f1e0f9f12345199338b50b126
Oleksandr Bieliakov <bel9k@ukr.net>
2019-03-12 20:53:04 +0100

    Added

---
+++ src/gui/werner/e01/p1/Person.java
@@ -0,0 +1,62 @@
+package gui.werner.e01.p1;
+
+public class Person {
+    private String name;
+    private Car car;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person buys(String make, int price) {
+        if(car == null) car = new Car(make, price);
+        else car = new Car(make, price, car);
+        return this;
+    }
+
+    public String getName() {return name;}
+
+    public void showCars() {
+        if (car != null) car.showCars();
+        else System.out.print("No cars owned");
+    }
+    public void showCarsRev() {
+        if (car != null) car.showCarsRev();
+        else System.out.print("No cars owned");
+    }
+
+    public int getTotalPrice() {
+        int sum = 0;
+        Car tmp = car;
+        while (tmp != null) {
+            sum += tmp.getPrice();
+            tmp = tmp.getNext();
+        }
+        return sum;
+    }
+
+    public boolean hasCar(String make) {
+        Car tmp = car;
+        while (tmp != null) {
+            if(tmp.getMake().equalsIgnoreCase(make))
+                return true;
+            tmp = tmp.getNext();
+        }
+        return false;
+    }
+
+    public Car mostExpensive() {
+        Car mostExp = null;
+        int price = 0;
+        Car tmp = car;
+        while (tmp != null) {
+            if(tmp.getPrice() > price) {
+                mostExp = tmp;
+                price = mostExp.getPrice();
+            }
+            tmp = tmp.getNext();
+        }
+        return mostExp;
+    }
+
+}
