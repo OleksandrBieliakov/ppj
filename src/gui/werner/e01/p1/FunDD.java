18acba6a37113c3bc211395bc1a7f4cb5f09975f
Oleksandr Bieliakov <bel9k@ukr.net>
2019-03-26 22:24:00 +0100

    Fixed

--- src/gui/werner/e01/p1/FunDD.java
+++ src/gui/werner/e01/p1/FunDD.java
@@ -7,10 +7,13 @@ interface FunDD {
     static double xminim(FunDD f, double a, double b) {
         double minX = a;
         double minY = f.fun(a);
-        for (double i = a + 1e-5; i <= b; i += 1e-5) {
-            if (f.fun(i) < minY)
-                minX = i;
-            minY = f.fun(i);
+        double y;
+        for (double x = a + 1e-5; x <= b; x += 1e-5) {
+            y = f.fun(x);
+            if (y < minY) {
+                minX = x;
+                minY = y;
+            }
         }
         return minX;
     }
