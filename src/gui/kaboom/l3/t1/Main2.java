9591cf3580aada1a06937311bc898ff4df8ed2f9
Alexander Belyakov <avbelyakov@gmail.com>
2019-04-12 13:41:43 +0300

    refactored: method for threads creation introduced

--- src/gui/kaboom/l3/t1/Main2.java
+++ src/gui/kaboom/l3/t1/Main2.java
@@ -3,16 +3,26 @@ package gui.kaboom.l3.t1;
 public class Main2 {
 
     private static double doHardWork() {
-        return 1235.0;
+        double dummy = 0.0;
+        for (int i = 0; i < 1000; i++) {
+            dummy += i;
+        }
+        return dummy;
     }
 
     public static void main(String[] args) {
-        new Thread(() -> {
+        createThread("Evens", 0, 2).start();
+        createThread("Odds ", 1, 2).start();
+        createThread("All  ", 0, 1).start();
+    }
+
+    private static Thread createThread(String title, int from, int step) {
+        return new Thread(() -> {
             long start = System.currentTimeMillis();
             long c = 0;
             double x = 0.0;
             boolean toggle = false;
-            for (int i = 0; i < 100_000_000; ++i) {
+            for (int i = from; i < 1_000_000 + from; i += step) {
                 c++;
                 toggle = !toggle;
                 if (toggle)
@@ -20,41 +30,7 @@ public class Main2 {
                 else
                     x -= doHardWork();
             }
-            System.out.println("\nLoop: " + c + " : " + x + " : " + (System.currentTimeMillis() - start));
-        }).start();
-
-        new Thread(() -> {
-            long start = System.currentTimeMillis();
-            long c = 0;
-            double x = 0.0;
-            boolean toggle = false;
-            for (int i = 0; i < 100_000_000; i += 2) {
-                c++;
-                toggle = !toggle;
-                if (toggle)
-                    x += doHardWork();
-                else
-                    x -= doHardWork();
-            }
-            System.out.println("\nEvens: " + c + " : " + x + " : " + (System.currentTimeMillis() - start));
-        }).start();
-
-        new Thread(() -> {
-            long start = System.currentTimeMillis();
-            long c = 0;
-            double x = 0.0;
-            boolean toggle = false;
-            for (int i = 1; i <= 100_000_000; i += 2) {
-                c++;
-                toggle = !toggle;
-                if (toggle)
-                    x += doHardWork();
-                else
-                    x -= doHardWork();
-            }
-            System.out.println("\nOdds: " + c + " : " + x + " : " + (System.currentTimeMillis() - start));
-        }).start();
-
-
+            System.out.println(title + " : " + c + " : " + x + " : " + (System.currentTimeMillis() - start));
+        });
     }
 }
