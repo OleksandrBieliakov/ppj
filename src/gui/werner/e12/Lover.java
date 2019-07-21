1ae1bbb0fa146d47582b65b50736c54470454ffc
Oleksandr Bieliakov <bel9k@ukr.net>
2019-07-21 18:51:14 +0200

    Message clear on esc implemented

--- src/gui/werner/e12/Lover.java
+++ src/gui/werner/e12/Lover.java
@@ -2,9 +2,12 @@ package gui.werner.e12;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
@@ -16,6 +19,7 @@ class Lover extends VBox {
     private Lover partner;
     private TextArea history = new TextArea();
     private BorderPane inputPane = new BorderPane();
+    private Label nameLabel = new Label();
     private TextField message = new TextField();
     private Button sendB = new Button("Send");
     private EventHandler<ActionEvent> sending;
@@ -26,23 +30,32 @@ class Lover extends VBox {
         history.setPrefRowCount(25);
         history.setPrefColumnCount(30);
 
-        message.setText(name + ": ");
         message.setPrefColumnCount(30);
 
         sending = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                dialog = dialog + "\n" + message.getText();
-                message.setText(name + ": ");
+                dialog = dialog + name + ": " + message.getText() + "\n";
+                message.setText("");
                 history.setText(dialog);
                 partner.recieveMessage();
             }
         };
 
+        message.setOnKeyPressed(e-> {
+            if(e.getCode() == KeyCode.ESCAPE) {
+                message.setText("");
+            }
+        });
+
         message.setOnAction(sending);
         sendB.setOnAction(sending);
 
-        inputPane.setLeft(message);
+        nameLabel.setText(name + ":");
+        nameLabel.setPadding(new Insets(5));
+
+        inputPane.setLeft(nameLabel);
+        inputPane.setCenter(message);
         inputPane.setRight(sendB);
 
         getChildren().addAll(history, inputPane);
