d84e8154b9ccfbd49dd5a766836f273dd8604128
Oleksandr Bieliakov <abel9k@gmail.com>
2019-11-05 18:49:11 +0100

    Analyser skeleton implemented

--- src/daftacademy/Analyser.java
+++ src/daftacademy/Analyser.java
@@ -1,7 +1,27 @@
 package daftacademy;
 
+import java.util.List;
+
 public class Analyser {
 
+    public static int numberOfContainersDeliveredToCountry(List<Ship> ships, String country) {
+        return 0;
+    }
 
+    public static String shipCategoryCarryingMostContainers(List<Ship> ships) {
+        return "";
+    }
+
+    public static int averageContainerWeightWith(List<Ship> ships, String material) {
+        return 0;
+    }
+
+    public static String firmFromCountrySentMostContainers(List<Ship> ships, String country) {
+        return "";
+    }
+
+    public static String mostExpensiveCargoTypeExportedFromCountryByItsFirms(List<Ship> ships, String country) {
+        return "";
+    }
 
 }
--- src/daftacademy/Container.java
+++ src/daftacademy/Container.java
@@ -4,11 +4,11 @@ import java.util.Objects;
 
 public class Container {
 
-    private String from_country;
-    private String to_country;
+    private String fromCountry;
+    private String toCountry;
     private String id;
     private int weight;
-    private String cargo_type;
+    private String cargoType;
     private String firm;
     private String firmName;
     private String firmCounty;
@@ -16,23 +16,23 @@ public class Container {
 
     public Container(String from_country, String to_country, String id, int weight, String cargo_type,
                      String firm, String firmName, String firmCounty, double price) {
-        this.from_country = from_country;
-        this.to_country = to_country;
+        this.fromCountry = from_country;
+        this.toCountry = to_country;
         this.id = id;
         this.weight = weight;
-        this.cargo_type = cargo_type;
+        this.cargoType = cargo_type;
         this.firm = firm;
         this.firmName = firmName;
         this.firmCounty = firmCounty;
         this.price = price;
     }
 
-    public String getFrom_country() {
-        return from_country;
+    public String getFromCountry() {
+        return fromCountry;
     }
 
-    public String getTo_country() {
-        return to_country;
+    public String getToCountry() {
+        return toCountry;
     }
 
     public String getId() {
@@ -43,8 +43,8 @@ public class Container {
         return weight;
     }
 
-    public String getCargo_type() {
-        return cargo_type;
+    public String getCargoType() {
+        return cargoType;
     }
 
     public String getFirm() {
@@ -70,10 +70,10 @@ public class Container {
         Container container = (Container) o;
         return getWeight() == container.getWeight() &&
                 Double.compare(container.getPrice(), getPrice()) == 0 &&
-                Objects.equals(getFrom_country(), container.getFrom_country()) &&
-                Objects.equals(getTo_country(), container.getTo_country()) &&
+                Objects.equals(getFromCountry(), container.getFromCountry()) &&
+                Objects.equals(getToCountry(), container.getToCountry()) &&
                 Objects.equals(getId(), container.getId()) &&
-                Objects.equals(getCargo_type(), container.getCargo_type()) &&
+                Objects.equals(getCargoType(), container.getCargoType()) &&
                 Objects.equals(getFirm(), container.getFirm()) &&
                 Objects.equals(getFirmName(), container.getFirmName()) &&
                 Objects.equals(getFirmCounty(), container.getFirmCounty());
@@ -81,18 +81,18 @@ public class Container {
 
     @Override
     public int hashCode() {
-        return Objects.hash(getFrom_country(), getTo_country(), getId(), getWeight(), getCargo_type(),
+        return Objects.hash(getFromCountry(), getToCountry(), getId(), getWeight(), getCargoType(),
                 getFirm(), getFirmName(), getFirmCounty(), getPrice());
     }
 
     @Override
     public String toString() {
         return "Container{" +
-                "from_country='" + from_country + '\'' +
-                ", to_country='" + to_country + '\'' +
+                "from_country='" + fromCountry + '\'' +
+                ", to_country='" + toCountry + '\'' +
                 ", id='" + id + '\'' +
                 ", weight=" + weight +
-                ", cargo_type='" + cargo_type + '\'' +
+                ", cargo_type='" + cargoType + '\'' +
                 ", firm='" + firm + '\'' +
                 ", firmName='" + firmName + '\'' +
                 ", firmCounty='" + firmCounty + '\'' +
